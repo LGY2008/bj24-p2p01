4ab11c6f96429c92e158613f9ad89892ddec3eea
monkey666 <email_test@163.com>
2020-10-23 11:52:18 +0800

    sys

--- run_suite.py
+++ run_suite.py
@@ -1,9 +1,13 @@
 # coding:utf-8
 # 1、 导包
 import unittest
+import os, sys
+
+sys.path.append(os.getcwd())
 from lib.HTMLTestRunner_HeiMa import HTMLTestRunner
+
 # 2、定义测试套件
 suite = unittest.defaultTestLoader.discover("./scripts")
 # 3、实例化HTMLTestRunner类并调用run方法
-with open("./report/report.html","wb")as f:
+with open("./report/report.html", "wb")as f:
     HTMLTestRunner(stream=f).run(suite)
